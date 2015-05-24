6f8aee28832215fa9916187d73c40d6098a7ee2a
Ben Neely <ben@bloc.io>
2015-05-24 14:36:32 -0600

    Complete Searching

--- models/address_book.rb
+++ models/address_book.rb
@@ -29,5 +29,25 @@ class AddressBook
 
     return csv.count
   end
-end
 
+  # Search AddressBook for a specific entry by name
+  def binary_search(name)
+     lower = 0
+     upper = entries.length - 1
+
+     while lower <= upper
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+ 
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+
+     return nil
+  end
+end
