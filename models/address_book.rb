18d2d6dcc727003879776c3c5eecd46474e4f21e
robmadden <rob.madden@bloc.io>
2015-06-26 10:41:47 -0700

    Complete Searching

--- models/address_book.rb
+++ models/address_book.rb
@@ -27,5 +27,25 @@ class AddressBook
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
   end
-end
 
+  # Search AddressBook for a specific entry by name
+  def binary_search(name)
+     lower = 0
+     upper = entries.length - 1
+
+     while lower <= upper
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+ 
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+
+     return nil
+  end
+end
